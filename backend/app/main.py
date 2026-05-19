3f50e05d96269014d34bb1c4c25fa029061de284
Mario-GT488 <mgarc412@itam.mx>
2026-05-19 11:45:34 -0600

    Connect frontend with posts API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app import models
 from app.routers import posts
@@ -9,6 +10,14 @@ app = FastAPI(
     title = "Visualize API",
     description = "API para guardar y descubrir inspiración visual",
     version = "1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(posts.router)
---
+++ frontend/src/services/api.js
@@ -0,0 +1,60 @@
+const API_BASE_URL = "http://127.0.0.1:8000";
+
+export async function getPosts(page = 1, limit = 10){
+    const response = await fetch(`${API_BASE_URL}/v1/posts/?page=${page}&limit=${limit}`);
+
+    if(!response.ok){
+        throw new Error("Error al obtener los posts");
+    }
+
+    return response.json();
+}
+
+export async function createPost(postData, currentUser){
+    const response = await fetch(`${API_BASE_URL}/v1/posts/`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-User": currentUser
+        },
+        body: JSON.stringify(postData),
+    });
+
+    if(!response.ok){
+        throw new Error("Error al crear post");
+    };
+
+    return response.json();
+}
+
+export async function updatePost(postId, postData, currentUser){
+    const response = await fetch(`${API_BASE_URL}/v1/posts/${postId}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "X-User": currentUser,
+        },
+        body: JSON.stringify(postData)
+    });
+
+    if(!response.ok){
+        throw new Error("Error al editar el post");
+    }
+
+    return response.json();
+}
+
+export async function deletePost(postId, currentUser){
+    const response = await fetch(`${API_BASE_URL}/v1/posts/${postId}`, {
+        method: "DELETE",
+        headers: {
+            "X-User": currentUser
+        },
+    });
+
+    if(!response.ok){
+        throw new Error("Error al eliminar el post");
+    }
+
+    return response.json();
+}
