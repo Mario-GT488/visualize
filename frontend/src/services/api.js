3b6695e1c97f5b72e01d0061db1b4960cbc54e53
Mario-GT488 <mgarc412@itam.mx>
2026-05-19 19:20:09 -0600

    App deploy API configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 export async function getPosts(page = 1, limit = 10){
     const response = await fetch(`${API_BASE_URL}/v1/posts/?page=${page}&limit=${limit}`);
